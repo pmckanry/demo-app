e3bc706afb4b5cd92f6c7d305966783dff2d5b5f
Patric McKanry <patric.mckanry@gmail.com>
2020-02-11 22:27:57 -0600

    Fix brewery route to ensure response.

--- server/routes/breweries.js
+++ server/routes/breweries.js
@@ -7,19 +7,25 @@ const breweriesRouter = new Router();
 // Do we want a service layer here?
 // Router => Service => DAO?
 breweriesRouter.get('/', async (req, res) => {
-    const params = req.params;
-
-    const query = {
-        type: req.queryString('type'),
-        state: req.queryString('state')
-    };
-
-    const breweries = await breweriesDao.getBreweries(query);
-
-    return res.json({
-        count: breweries.length,
-        breweries
-    })
+    try {
+        const query = {
+            type: req.queryString('type'),
+            state: req.queryString('state')
+        };
+    
+        const breweries = await breweriesDao.getBreweries(query);
+    
+        return res.status(200).json({
+            count: breweries.length,
+            breweries
+        });
+    }
+    catch(err) {
+        return res.status(500).json({
+            error: true,
+            message: err.toString()
+        });
+    }
 });
 
 module.exports = breweriesRouter;
